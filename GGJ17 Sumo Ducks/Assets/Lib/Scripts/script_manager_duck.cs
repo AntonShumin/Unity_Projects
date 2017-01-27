73fb6e68c67e69156b748d240665a8053f1f41fb
AntonShumin <AntonShuminBe@gmail.com>
2017-01-27 23:40:08 +0100

    write duckmanagers

--- GGJ17 Sumo Ducks/Assets/Lib/Scripts/script_manager_duck.cs
+++ GGJ17 Sumo Ducks/Assets/Lib/Scripts/script_manager_duck.cs
@@ -1,22 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class script_manager_duck : MonoBehaviour {
+[Serializable]
+public class script_manager_duck{
 
     public Color m_PlayerColor;
     public Transform m_SpownPoint;
-    [HideInInspector]
-    public int m_PlayerNumber;
-    [HideInInspector]
-    public string m_ColoredPlayerText;
-    [HideInInspector]
-    public GameObject m_Instance;
-    [HideInInspector]
-    public int m_Wins;
 
+    [HideInInspector]    public int m_PlayerNumber;
+    [HideInInspector]    public string m_ColoredPlayerText;
+    [HideInInspector]    public GameObject m_Instance;
+    [HideInInspector]    public int m_Wins;
+
+    private GameObject m_canvas_element;
     private script_movement m_script_movement;
     private Collider m_collider;
     private Rigidbody m_rigidbody;
+
+    public void Setup()
+    {
+        m_script_movement = m_Instance.GetComponent<script_movement>();
+
+        m_script_movement.m_PlayerNumber = m_PlayerNumber;
+
+        m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
+    }
+
+    public void DisableControl()
+    {
+        m_script_movement.enabled = false;
+
+  
+    }
+
+    public void EnableControl()
+    {
+        m_script_movement.enabled = true;
+    }
+
+    public void Reset()
+    {
+        m_Instance.transform.position = m_SpownPoint.position;
+        m_Instance.transform.rotation = m_SpownPoint.rotation;
+    }
 
 }
--- GGJ17 Sumo Ducks/Assets/Lib/Scripts/script_manager_game.cs
+++ GGJ17 Sumo Ducks/Assets/Lib/Scripts/script_manager_game.cs
@@ -9,7 +9,47 @@ public class script_manager_game : MonoBehaviour {
     public float m_end_delay;
     public script_camera m_script_camera;
     public GameObject m_prefab_duck;
+    public script_manager_duck[] m_script_ducks;
 
     private int m_rounds_current;
+    private WaitForSeconds m_StartWait;
+    private WaitForSeconds m_EndWait;
+    private script_manager_duck m_RoundWinner;
+    private script_manager_duck m_GameWinner;
+
+    private void Start()
+    {
+        m_StartWait = new WaitForSeconds(m_start_delay);
+        m_EndWait = new WaitForSeconds(m_end_delay);
+
+        Spawn_All_Ducks();
+        Set_Camera_Targets();
+    }
+
+    private void Spawn_All_Ducks()
+    {
+        for (int i = 0; i < m_script_ducks.Length; i++ )
+        {
+            m_script_ducks[i].m_Instance = Instantiate(m_prefab_duck, m_script_ducks[i].m_SpownPoint.position, m_script_ducks[i].m_SpownPoint.rotation) as GameObject;
+            m_script_ducks[i].m_PlayerNumber = i + 1;
+            m_script_ducks[i].Setup();    
+        } 
+    }
+
+    private void Set_Camera_Targets()
+    {
+        //Preset vaar
+        Transform[] targets = new Transform[m_script_ducks.Length];
+
+        //colleect targets
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targets[i] = m_script_ducks[i].m_Instance.transform;
+        }
+
+        //set targets
+        m_script_camera.m_Targets = targets;
+
+    }
 
 }
