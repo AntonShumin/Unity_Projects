48e15b4613cca1ab2b6b6168ac80bb0d19d3ae90
AntonShumin <AntonShuminBe@gmail.com>
2017-02-05 01:24:32 +0100

    new wave compound collider

--- GGJ17 Sumo Ducks/Assets/Lib/Scripts/script_wave.cs
+++ GGJ17 Sumo Ducks/Assets/Lib/Scripts/script_wave.cs
@@ -13,18 +13,49 @@ public class script_wave : MonoBehaviour {
     {
         m_Player_Number = player_number;
         m_origin = position;
-        position.y = 0.4f;
+        position.y = -1f;
         transform.position = position;
-        transform.localScale = new Vector3(3, 3, 10);
+        transform.localScale = new Vector3(3, 3, 3);
+        m_target_list.Clear();
     }
 
     void Update()
     {
-        transform.localScale += new Vector3(50, 50, 10f) * Time.deltaTime;
-        if(transform.localScale.x > 100f)
+        transform.localScale += new Vector3(80, 80, 5f) * Time.deltaTime;
+        if(transform.localScale.x > 130f)
         {
             gameObject.SetActive(false);
         }
+    }
+
+    public void Collider_Event(Collider col)
+    {
+        GameObject target = col.gameObject;
+        if (col.gameObject.tag == "Player")
+        {
+            //has a movement script
+            if (target.GetComponent<script_movement>() != null)
+            {
+                //if a different player
+                if (target.GetComponent<script_movement>().m_PlayerNumber != m_Player_Number)
+                {
+                    if (!m_target_list.Contains(target))
+                    {
+                        m_target_list.Add(target);
+
+                        Target_Push(col.gameObject);
+                    }
+
+
+                }
+            }
+        }
+    }
+
+    private void Target_Push(GameObject target)
+    {
+        Vector3 force_vector = Vector3.Normalize(target.transform.position - m_origin) * 10000000;
+        target.GetComponent<Rigidbody>().AddForce(force_vector);
     }
 
 
---
+++ GGJ17 Sumo Ducks/Assets/Lib/Scripts/script_wave_collider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_wave_collider : MonoBehaviour {
+
+    private script_wave m_parent;
+
+    void Start()
+    {
+        m_parent = transform.GetComponentInParent<script_wave>();
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        m_parent.Collider_Event(col);
+    }
+}
