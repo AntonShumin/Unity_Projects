989ac2cc807d8b677e933eedd278610c610c9fa5
AntonShumin <AntonShuminBe@gmail.com>
2017-09-01 01:26:14 +0200

    hover boxes

---
+++ Hover_Boxes/Assets/scripts/script_block.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class script_block : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    //vars
+    private Vector3 m_position_default;
+    private RectTransform m_rectTransform;
+
+    //cached
+    private Vector3 c_position_desired = new Vector3(0,0,0);
+    private Vector3 c_position_start = new Vector3(0, 0, 0);
+    private Vector2 c_scale_desired = new Vector2(0, 0);
+    private Vector2 c_scale_start = new Vector2(0, 0);
+    private float c_running_time;
+    private float c_time_desired;
+
+    void Awake()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+        m_position_default = m_rectTransform.transform.position; //store default position
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(Scale(1));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(Scale(2));
+    }
+
+    IEnumerator Scale(int type)
+    {
+
+        switch (type)
+        {
+            case 1: //bigger
+                c_time_desired = 0.1f;
+                c_scale_desired.x = c_scale_desired.y = script_manager.m_block_size * 2;
+                c_position_desired.x = m_position_default.x - script_manager.m_block_size / 2;
+                c_position_desired.y = m_position_default.y + script_manager.m_block_size / 2;
+                m_rectTransform.SetAsLastSibling(); //max z index
+                break;
+            case 2: //smaller
+                c_time_desired = 1f;
+                c_scale_desired.x = c_scale_desired.y = script_manager.m_block_size;
+                c_position_desired.x = m_position_default.x;
+                c_position_desired.y = m_position_default.y;
+                break;
+            default: //fallback
+                yield break;
+        }
+
+        //preset vars
+        c_running_time = 0f;
+        c_scale_start = m_rectTransform.sizeDelta;
+        c_position_start = m_rectTransform.position;
+
+        //interate
+        do
+        {
+            m_rectTransform.sizeDelta = Vector2.Lerp(c_scale_start, c_scale_desired, c_running_time / c_time_desired);
+            m_rectTransform.transform.position = Vector3.Lerp(c_position_start, c_position_desired, c_running_time / c_time_desired);
+            c_running_time += Time.deltaTime;
+            yield return null;
+
+        } while (c_running_time <= c_time_desired);
+    }
+
+}
---
+++ Hover_Boxes/Assets/scripts/script_manager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class script_manager : MonoBehaviour {
+
+    //vars
+    public GameObject m_block_prefab;
+    public static float m_block_size;
+
+    //setup
+    void Awake()
+    {
+        //screen orientation
+        Screen.orientation = ScreenOrientation.Portrait;
+
+        //block size
+        m_block_size = Screen.width / 10;
+        m_block_prefab.GetComponent<RectTransform>().sizeDelta = new Vector2(m_block_size, m_block_size);
+
+        //start
+        Build_Grid();
+
+    }
+
+    //build block grid
+    private void Build_Grid() {
+
+        for (int x = 0; x < Screen.width / m_block_size ; x++)
+        {
+
+            for (int y = 1; y <= Screen.height / m_block_size + 1; y++)
+            {
+
+                Vector3 position = new Vector3(x * m_block_size, y * m_block_size, 0);
+                GameObject block = Instantiate(m_block_prefab, position, Quaternion.identity, gameObject.transform);
+                block.GetComponent<Image>().color = Random.ColorHSV(0f, 1f, 0f, 1f, 0.5f, 1f);
+
+            }
+        }
+
+    }
+}
